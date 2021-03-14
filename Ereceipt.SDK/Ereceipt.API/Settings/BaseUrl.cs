67f2404625a6d72d8e4b5de3f01d742403346417
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-14 12:08:55 +0200

    Init project

---
+++ Ereceipt.SDK/Ereceipt.API/Settings/BaseUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ereceipt.API.Settings
+{
+    public class BaseUrl
+    {
+        private const string LocalHost = "https://localhost:5001/api/v1";
+
+        private string groups;
+        private string users;
+        private string identity;
+        private string receipts;
+
+        public BaseUrl()
+        {
+            groups = $"{LocalHost}/groups";
+            users = $"{LocalHost}/users";
+            identity = $"{LocalHost}/identity";
+            receipts = $"{LocalHost}/receipts";
+        }
+    }
+}
