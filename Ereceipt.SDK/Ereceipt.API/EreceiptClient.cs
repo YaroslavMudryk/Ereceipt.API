b7e463a9c9e98c8bd9937dd9141832faefb12cb8
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-14 12:59:59 +0200

    Added main client and User model

---
+++ Ereceipt.SDK/Ereceipt.API/EreceiptClient.cs
@@ -0,0 +1,35 @@
+﻿using Ereceipt.API.Services.Interfaces;
+using Ereceipt.API.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ereceipt.API
+{
+    public class EreceiptClient
+    {
+        private string accessToken;
+        private int id;
+        private BaseUrl urls;
+
+        private IGroupService groupService;
+        private IIdentityService identityService;
+        private IUserService userService;
+        private IReceiptService receiptService;
+
+        public EreceiptClient(string accessToken)
+        {
+            this.accessToken = accessToken;
+            urls = new BaseUrl();
+        }
+
+        public EreceiptClient(IGroupService groupService, IIdentityService identityService, IUserService userService, IReceiptService receiptService)
+        {
+            this.groupService = groupService;
+            this.identityService = identityService;
+            this.userService = userService;
+            this.receiptService = receiptService;
+        }
+    }
+}
