1051aaa807e9c2611757816e74119f2ccd7ece75
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-04-27 15:21:29 +0300

    Added FileStorage as implementations

--- Ereceipt.SDK/Ereceipt.API/Storage/IStorage.cs
+++ Ereceipt.SDK/Ereceipt.API/Storage/IStorage.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Ereceipt.API.Constants;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,20 @@ namespace Ereceipt.API.Storage
     {
         Task DownloadAsync(string content);
         Task<string> UploadAsync(); 
+    }
+
+    public class FileStorage : IStorage
+    {
+        public async Task DownloadAsync(string content)
+        {
+            using var sw = new StreamWriter(TokenConstants.FullTokenPath);
+            await sw.WriteLineAsync(content);
+        }
+
+        public async Task<string> UploadAsync()
+        {
+            using var sr = new StreamReader(TokenConstants.FullTokenPath);
+            return await sr.ReadLineAsync();
+        }
     }
 }
